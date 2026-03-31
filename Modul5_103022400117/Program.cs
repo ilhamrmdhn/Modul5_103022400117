50fe3c115b7d14784f5b3065da67d6d6e11c24cb
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-03-31 08:13:52 +0700

    implementasi generic method

--- Modul5_103022400117/Program.cs
+++ Modul5_103022400117/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        DateTime currentTime = DateTime.Now;
+        inputDates.Add(currentTime);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine($"Data {i + 1} berisi: {storedData[i]}, yang disimpan pada waktu: {inputDates[i]}");
+        }
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        SimpleDataBase<int> data = new SimpleDataBase<int>();
+        data.AddNewData(10);
+        data.AddNewData(20);
+        data.AddNewData(30);
+        data.PrintAllData();
+    }
+}
